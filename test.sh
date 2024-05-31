b5dc741b1ca4cce47164c34d6bb31ea10ffa1d8f
Joel Ek <contact@joelek.se>
2024-05-31 09:19:26 +0200

    Update test script.

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-ffmpeg -hide_banner -loglevel panic -i ./samples/uniform.png -vf "format=yuv420p16le" -f rawvideo pipe: |
-	./build/denice yuv420p16le 300 300 0.1 |
-	ffmpeg -hide_banner -loglevel panic -f rawvideo -pix_fmt yuv420p16le -s 300:300 -i pipe: -vf "format=rgb24" -y ./samples/uniform.010.png;
+ffmpeg -hide_banner -loglevel panic -i ./samples/simpsons.png -vf "format=yuv420p16le" -f rawvideo pipe: |
+	./build/denice yuv420p16le 720 576 0.10 |
+	ffmpeg -hide_banner -loglevel panic -f rawvideo -pix_fmt yuv420p16le -s 720:576 -i pipe: -vf "format=rgb24" -y ./samples/simpsons.010.yuv420p16le.png;
+
+ffmpeg -hide_banner -loglevel panic -i ./samples/simpsons.png -vf "format=yuv422p16le" -f rawvideo pipe: |
+	./build/denice yuv422p16le 720 576 0.10 |
+	ffmpeg -hide_banner -loglevel panic -f rawvideo -pix_fmt yuv422p16le -s 720:576 -i pipe: -vf "format=rgb24" -y ./samples/simpsons.010.yuv422p16le.png;
+
+ffmpeg -hide_banner -loglevel panic -i ./samples/simpsons.png -vf "format=yuv444p16le" -f rawvideo pipe: |
+	./build/denice yuv444p16le 720 576 0.10 |
+	ffmpeg -hide_banner -loglevel panic -f rawvideo -pix_fmt yuv444p16le -s 720:576 -i pipe: -vf "format=rgb24" -y ./samples/simpsons.010.yuv444p16le.png;
