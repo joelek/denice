2e83d6e51ceeb4d76c08bfd95b12400da3d63624
Joel Ek <contact@joelek.se>
2024-12-05 20:15:09 +0100

    Move source files to source folder.

--- make.sh
+++ make.sh
@@ -7,5 +7,5 @@ PATH_LIBRARY="";
 LINKED_LIBRARIES="-l stdc++ -l OpenCL";
 
 mkdir -p build
-gcc $COMPILER_OPTIONS $COMPILER_DEFINES -I "$PATH_INCLUDE" -c ./dct_denoise.cpp -o ./build/dct_denoise.o -L "$PATH_LIBRARY" $LINKED_LIBRARIES;
-gcc $COMPILER_OPTIONS $COMPILER_DEFINES -I "$PATH_INCLUDE" ./build/dct_denoise.o denice.cpp -o ./build/denice -L "$PATH_LIBRARY" $LINKED_LIBRARIES;
+gcc $COMPILER_OPTIONS $COMPILER_DEFINES -I "$PATH_INCLUDE" -c ./source/dct_denoise.cpp -o ./build/dct_denoise.o -L "$PATH_LIBRARY" $LINKED_LIBRARIES;
+gcc $COMPILER_OPTIONS $COMPILER_DEFINES -I "$PATH_INCLUDE" ./build/dct_denoise.o source/denice.cpp -o ./build/denice -L "$PATH_LIBRARY" $LINKED_LIBRARIES;
