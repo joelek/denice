ef6c7a534ca4d2487338c361a5f733cd3b2f7939
Joel Ek <contact@joelek.se>
2024-12-05 20:25:03 +0100

    Add versioning script.

---
+++ version.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+source ./version.env
+
+if [ $# -eq 1 ] && ([ $1 = "major" ] || [ $1 = "minor" ] || [ $1 = "patch" ]); then
+	echo "[updating version]"
+else
+	echo "usage: version <major|minor|patch>"
+	exit 1
+fi
+
+echo "old version: $MAJOR.$MINOR.$PATCH"
+
+if [ $1 = "major" ]; then
+	MAJOR=$((MAJOR+1))
+	MINOR=0
+	PATCH=0
+elif [ $1 = "minor" ]; then
+	MAJOR=$((MAJOR+0))
+	MINOR=$((MINOR+1))
+	PATCH=0
+elif [ $1 = "patch" ]; then
+	MAJOR=$((MAJOR+0))
+	MINOR=$((MINOR+0))
+	PATCH=$((PATCH+1))
+fi
+
+echo "new version: $MAJOR.$MINOR.$PATCH"
+
+rm version.env
+echo "MAJOR=$MAJOR" >> version.env
+echo "MINOR=$MINOR" >> version.env
+echo "PATCH=$PATCH" >> version.env
+
+git add version.env
+git commit -m "$MAJOR.$MINOR.$PATCH"
+git tag -a "v$MAJOR.$MINOR.$PATCH" -m "$MAJOR.$MINOR.$PATCH"
+git push && git push --tags
